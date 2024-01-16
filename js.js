24f20ef06b366e7dc102f491c06fd173a01184b9
Brandon Ngo <Brandon.ngo.inbox@gmail.com>
2024-01-15 22:38:14 -0600

    update buttons, and add event listener

--- js.js
+++ js.js
@@ -1,3 +1,20 @@
-function redirectToPhoPage() {
-    window.location.href = "phoRecipe.html"; 
-}
+document.addEventListener('DOMContentLoaded', function() {
+  const phoRecipePage = document.querySelector('#phoRecipePage');
+  const homePage = document.querySelector('#homePage');
+
+  if (phoRecipePage) {
+    phoRecipePage.addEventListener("click", reDirect);
+  }
+
+  if (homePage) {
+    homePage.addEventListener("click", goBack);
+  }
+
+  function reDirect() {
+    window.location.href = 'phoRecipe.html';
+  }
+
+  function goBack() {
+    window.location.href = 'homePage.html';
+  }
+});
