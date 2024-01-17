902a47135ce33fb243150062929d9dcda47c950c
Brandon Ngo <Brandon.ngo.inbox@gmail.com>
2024-01-17 15:02:24 -0600

    add button and event listener for Buna Cha

--- js.js
+++ js.js
@@ -1,18 +1,23 @@
 document.addEventListener('DOMContentLoaded', function() {
   const phoRecipePage = document.querySelector('#phoRecipePage');
-  const homePage = document.querySelector('#homePage');
   const springRollRecipePage = document.querySelector('#springRollRecipePage');
+  const BunaChaRecipePage = document.querySelector('#BunaChaRecipePage');
+  const homePage = document.querySelector('#homePage');
 
   if (phoRecipePage) {
     phoRecipePage.addEventListener("click", reDirectToPhoPage);
   }
 
-  if (homePage) {
-    homePage.addEventListener("click", goBackHome);
-  }
-
   if (springRollRecipePage) {
     springRollRecipePage.addEventListener("click", reDirectToSpringRollPage)
+  }
+
+  if (BunaChaRecipePage) {
+    BunaChaRecipePage.addEventListener("click", reDirectToBunaChaRecipePage)
+  }
+
+  if (homePage) {
+    homePage.addEventListener("click", goBackHome);
   }
 
   function reDirectToPhoPage() {
@@ -22,6 +27,11 @@ document.addEventListener('DOMContentLoaded', function() {
   function reDirectToSpringRollPage() {
     window.location.href = 'springRollRecipe.html'
   }
+
+  function reDirectToBunaChaRecipePage() {
+    window.location.href = 'BunaChaRecipe.html'
+  }
+
   function goBackHome() {
     window.location.href = 'homePage.html';
   }
